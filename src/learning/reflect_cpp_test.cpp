5e4acedf8f564bdb5d46c5f67beaa97c3a2eb9a2
hankrof <hankrof@gmail.com>
2024-08-07 23:43:09 +0800

    [modules] Add reflect-cpp modules

---
+++ src/learning/reflect_cpp_test.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include <rfl.hpp>
+TEST(ReflectCppTest, HelloWorld)
+{
+    struct Person {
+        std::string first_name;
+        std::string last_name;
+        int age;
+    };
+
+    const auto homer =
+        Person{.first_name = "Homer",
+               .last_name = "Simpson",
+               .age = 45};
+
+    const std::string json_string = rfl::json::write(homer);
+    auto homer2 = rfl::json::read<Person>(json_string).value();
+}
