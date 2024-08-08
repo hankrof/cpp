3ea0fdf64fe3164b6fb7cea5866921a2c6a18779
hankrof <hankrof@gmail.com>
2024-08-08 22:08:59 +0800

    [reflect-cpp] test how to reflect from a file
    reflect from a json file.

--- src/learning/reflect_cpp_test.cpp
+++ src/learning/reflect_cpp_test.cpp
@@ -1,12 +1,29 @@
 #include <gtest/gtest.h>
 #include <rfl.hpp>
 #include <rfl/json.hpp>
+#include <filesystem>
 #include "domain/person.h"
 
 TEST(ReflectCppTest, ReflectFromJsonString)
 {
     const auto homer = domain::Person {"Homer", "Simpson", 45};
-    const std::string json_string = rfl::json::write(homer);
-    auto reflHomer = rfl::json::read<domain::Person>(json_string).value();
+    const std::string jsonStr = rfl::json::write(homer);
+    auto reflHomer = rfl::json::read<domain::Person>(jsonStr).value();
     ASSERT_EQ(homer, reflHomer);
+}
+
+TEST(ReflectCppTest, ReflectFromJsonFile)
+{
+    const std::string str = "{\"firstName\":\"Homer\",\"lastName\":\"Simpson\",\"age\":45}";
+    const std::string filename = "homer.json";
+
+    std::ofstream outputFile(filename);
+    outputFile << str;
+    outputFile.close();
+
+    std::ifstream inputFile(filename);
+    auto homer = rfl::json::read<domain::Person>(inputFile).value();
+    ASSERT_EQ(rfl::json::write(homer), str);
+
+    std::filesystem::remove(filename);
 }
